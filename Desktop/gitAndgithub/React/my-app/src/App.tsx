6f83566148a3d7e576ff40880c39ab97e70b3f42
shahadM121 <smaalshabani@gmail.com>
2023-08-21 12:17:19 +0300

    setup gh-pages

---
+++ Desktop/gitAndgithub/React/my-app/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Test from "./component/Test";
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello my first react App</h1>
+    <Test></Test>
+    </>
+  )
+}
+
+
---
+++ Desktop/gitAndgithub/React/my-app/src/component/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>Shahad</div>
+  )
+}
+//rfc
+//rfce
+
+
